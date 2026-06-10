4a2483bf80d0bd40eb3e2174791b11d4e04e661a
Nikita <nikitag2709@gmail.com>
2026-06-10 17:24:13 +0200

    fixes

--- src/de/tum/ise/entities/Student.java
+++ src/de/tum/ise/entities/Student.java
@@ -1,9 +1,75 @@
 package de.tum.ise.entities;
 
-public class Student extends Person{
+public class Student {
 
 
-    public Student(String matrNr, String name, int age, int semester, String email) {
-        super(matrNr, name, age, semester, email);
+    private String matrNr;
+    private String name;
+    private int age;
+    private int semester;
+    private String email;
+
+    // TODO 1.1 : Bob is not good at Object Orientated Programming - fix it
+  	// TODO 1.1 : Instead of implementing void setSemester(int i), implement void increaseSemester()
+
+    // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
+
+    public Student(String name, int age, String email, String matrNr, int semester) {
+        this.matrNr = matrNr;
+        this.name = name;
+        this.age = age;
+        this.semester = semester;
+        this.email = email;
+    }
+
+    public String getMatrNr() {
+        return matrNr;
+    }
+
+    public void setMatrNr(String matrNr) {
+        this.matrNr = matrNr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void increaseSemester() {
+        this.semester ++;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "matrNr='" + matrNr + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", semester=" + semester +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
