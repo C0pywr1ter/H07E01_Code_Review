8f8b82c41349ee5fe8000e0cdf7de8675fcf5056
Nikita <nikitag2709@gmail.com>
2026-06-10 17:10:32 +0200

    initial comit

--- src/de/tum/ise/entities/Professor.java
+++ src/de/tum/ise/entities/Professor.java
@@ -1,21 +1,9 @@
 package de.tum.ise.entities;
 
-public class Professor {
+public class Professor extends Person {
 
-    private String persNr;
-    public static String name;
-    private static int age;
-    private int semester;
-    private String email;
 
-    // TODO 1.1 : - Bob is not good at Object Orientated Programming - fix it
-
-    // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
-
-    public Professor(String name, int age, String email, String persNr) {
-        this.persNr = persNr;
-        Professor.name = name;
-        Professor.age = age;
-        this.email = email;
+    public Professor(String matrNr, String name, int age, int semester, String email) {
+        super(matrNr, name, age, semester, email);
     }
 }
--- src/de/tum/ise/entities/Student.java
+++ src/de/tum/ise/entities/Student.java
@@ -1,28 +1,9 @@
 package de.tum.ise.entities;
 
-public class Student {
+public class Student extends Person{
 
 
-    private String matrNr;
-    public static String name;
-    private static int age;
-    private int semester;
-    private String email;
-
-    // TODO 1.1 : Bob is not good at Object Orientated Programming - fix it
-  	// TODO 1.1 : Instead of implementing void setSemester(int i), implement void increaseSemester()
-
-    // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
-
-    public Student(String name, int age, String email, String matrNr, int semester) {
-        this.matrNr = matrNr;
-        Student.name = name;
-        Student.age = age;
-        this.semester = semester;
-        this.email = email;
-    }
-
-    public String getMatrNr() {
-        return matrNr;
+    public Student(String matrNr, String name, int age, int semester, String email) {
+        super(matrNr, name, age, semester, email);
     }
 }
